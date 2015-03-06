a82a0a338bd6ce7db2eb15c6e51dc350b97e240a
Safia Abdalla <seabdalla@gmail.com>
2015-03-05 18:27:12 -0600

    Added timestamp functionality to the Event class
    - Added preceding and seceding functions to Event class
    - Added check for same timestamp in same_event
    - Added tests for preceding and seceding functions
    - Modifeid tests for same_event

--- endive/event.py
+++ endive/event.py
@@ -1,8 +1,15 @@
 import helpers
 
 class Event():
-    def __init__(self, data):
+    def __init__(self, data, timestamp):
         self.data = data
+        self.timestamp = helpers.convert_to_epoch(timestamp)
 
     def same_event(self, other):
-        return self.data == other.data
+        return self.data == other.data and self.timestamp == other.timestamp
+
+    def preceding(self, other):
+        return self.timestamp < other.timestamp
+
+    def seceding(self, other):
+        return self.timestamp > other.timestamp
--- tests/test_event.py
+++ tests/test_event.py
@@ -1,8 +1,27 @@
 from endive.event import Event
+import datetime
 
 def test_same_event():
-    this = Event([1,2,3,4])
-    same = Event([1,2,3,4])
-    different = Event([5,6,7,8])
+    this = Event([1,2,3,4], datetime.datetime(2015, 3, 5, 0, 0))
+    same = Event([1,2,3,4], datetime.datetime(2015, 3, 5, 0, 0))
+    different_data = Event([5,6,7,8], datetime.datetime(2015, 3, 5, 0, 0))
+    different_date = Event([1,2,3,4], datetime.datetime(2014, 3, 5, 0, 0))
+    different = Event([9,10,11,12], datetime.datetime(2016, 4, 5, 0, 0))
     assert this.same_event(same) == True
+    assert this.same_event(different_data) == False
+    assert this.same_event(different_date) == False
     assert this.same_event(different) == False
+
+def test_preceding():
+    this = Event([1,2,3,4], datetime.datetime(2015, 3, 5, 0, 0))
+    before = Event([1,2,3,4], datetime.datetime(2015, 3, 4, 0, 0))
+    after = Event([1,2,3,4], datetime.datetime(2015, 3, 6, 0, 0))
+    assert this.preceding(before) == False
+    assert this.preceding(after) == True
+
+def test_seceding():
+    this = Event([1,2,3,4], datetime.datetime(2015, 3, 5, 0, 0))
+    before = Event([1,2,3,4], datetime.datetime(2015, 3, 4, 0, 0))
+    after = Event([1,2,3,4], datetime.datetime(2015, 3, 6, 0, 0))
+    assert this.seceding(before) == True
+    assert this.seceding(after) == False
